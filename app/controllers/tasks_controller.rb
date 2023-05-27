89c215d289b48a91cff247125b156049dab6854c
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-05-27 14:07:20 +0300

    added a create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,19 @@
 class TasksController < ApplicationController
-    def index
-        @tasks = Task.all
-        @task = Task.new
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_url, notice: "Завдання створено!" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
